c8f94a7c0ee2c4d9ac0b9f0c037dc6803983d2d3
Lasse Jantsch <lassejantsch@web.de>
2026-05-01 16:45:45 +0900

    Added custom nn.linear

--- linear_transformer/models/generic.py
+++ linear_transformer/models/generic.py
@@ -2,25 +2,28 @@ from __future__ import annotations
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 
-class FrozenLayerNorm(nn.Module):
+from linear_transformer.modules.linear import linear_sub, linear_add
 
-    def __init__(self, weight: nn.Parameter, bias: nn.Parameter | None, eps: float, normalized_shape: tuple[int, ...], frozen_norm: bool = False) -> None:
+class CustomLayerNorm(nn.Module):
+
+    def __init__(self, weight: nn.Parameter, bias: nn.Parameter | None, eps: float, normalized_shape: tuple[int, ...], is_linear: bool = False) -> None:
         super().__init__()
         self.weight = weight
         self.bias = bias
         self.eps = eps
         self.normalized_shape = normalized_shape
-        self.frozen_norm = frozen_norm
+        self.is_linear = is_linear
 
     @classmethod
-    def from_module(cls, m: nn.Module, **kwargs: dict | None) -> FrozenLayerNorm:
+    def from_module(cls, m: nn.Module, **kwargs: dict | None) -> CustomLayerNorm:
         return cls(
             weight=m.weight,
             bias=m.bias,
             eps=m.eps,
             normalized_shape=tuple(m.normalized_shape),
-            frozen_norm=kwargs.get('frozen_norm', False),
+            is_linear=kwargs.get('frozen_norm', False),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:  # (B, N, d_model)
@@ -31,12 +34,55 @@ class FrozenLayerNorm(nn.Module):
         var = x_f.var(dim=dims, keepdim=True, unbiased=False) 
         sigma = (var + self.eps).sqrt()
 
-        if self.frozen_norm:
-            out = (x_f - mean.detach()) / sigma.detach() * self.weight.float().detach()
+        # linearized
+        if self.is_linear:
+            mean_cent_x_f = linear_sub(x_f, mean.detach(), self.eps)
+            out = mean_cent_x_f / sigma.detach() * self.weight.float().detach()
             if self.bias is not None:
-                out = out + self.bias.float().detach()
-        else:
-            out = (x_f - mean) / sigma * self.weight.float()
-            if self.bias is not None:
-                out = out + self.bias.float()
+                out = linear_add(out, self.bias.float().detach(), self.eps)
+            return out.to(x.dtype)
+
+        # original
+        out = (x_f - mean) / sigma * self.weight.float()
+        if self.bias is not None:
+            out = out + self.bias.float()
         return out.to(x.dtype)
+    
+
+class CustomLinear(nn.Module):
+
+    def __init__(self, weight: nn.Parameter, bias: nn.Parameter | None, is_linear: bool = False, reversed: bool = False):
+        super().__init__()
+        self.weight = weight
+        self.bias = bias
+        self.is_linear = is_linear
+        self.reversed = reversed
+
+    @property
+    def weight_4_linear(self):
+        if self.reversed:
+            return self.weight.T
+        return self.weight
+
+    @classmethod
+    def from_module(cls, m: nn.Module, **kwargs: dict) -> 'CustomLinear':
+        reversed = False
+        if isinstance(m, nn.Conv1d):
+            reversed = True
+        return cls(
+            weight = m.weight,
+            bias = m.bias,
+            is_linear = kwargs.get('frozen_linear', False),
+            reversed = reversed
+        )
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # linearized
+        if self.is_linear:
+            out = F.linear(x, self.weight_4_linear.detach())
+            if self.bias is not None:
+                out = linear_add(out, self.bias.detach(), 1e-8)
+            return out
+            
+        # original    
+        return F.linear(x, self.weight_4_linear, self.bias)
